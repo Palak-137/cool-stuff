4d5cf019683a262425c069ae19c9ff10b7b3cc3b
Palak-137 <kumaripalak.aps@gmail.com>
2021-07-30 02:30:24 +0530

    final commit

--- src/components/task2/task2.js
+++ src/components/task2/task2.js
@@ -8,10 +8,10 @@ function task2() {
     let slides = [
       
         <div className="container">  <img class="image" src="https://picsum.photos/800/300/?random" alt="1"></img><h2 className="centered">Image 1</h2></div>,
-        <div className="container"><img  src="https://i.picsum.photos/id/25/800/303.jpg?hmac=AuHQVAJ8-EVjVe-BgBYX_IFXPoGHbQyiGe3ycqR5YeQ" alt="2" /><h2 className="centered">Image 2</h2></div>  ,
-        <div className="container"><img  src="https://i.picsum.photos/id/557/800/302.jpg?hmac=I2sXTgJ5sAjh-zgrkL-6_4sitqdZGHeynppNrANIGoA" alt="3" /> <h2 className="centered">Image 3</h2></div> ,
-        <div className="container"><img  src="https://i.picsum.photos/id/518/800/302.jpg?hmac=Ln1HWkqMXlgd6KergdperWbMQd5wTGSH0AFnzyTPEEs" alt="4" /> <h2 className="centered">Image 4</h2></div> ,
-        <div className="container"> <img src="https://i.picsum.photos/id/735/800/301.jpg?hmac=HSqNqSPknd68ZUnZm2Nd1zQ9xfXUnMUcfg3eSyV55bA" alt="5" /><h2 className="centered">Image 5</h2></div>   ];
+        <div className="container"><img class="image"  src="https://picsum.photos/800/303/?random" alt="2" /><h2 className="centered">Image 2</h2></div>  ,
+        <div className="container"><img class="image" src="https://picsum.photos/800/304/?random" alt="3" /> <h2 className="centered">Image 3</h2></div> ,
+        <div className="container"><img class="image" src="https://picsum.photos/800/302/?random" alt="4" /> <h2 className="centered">Image 4</h2></div> ,
+        <div className="container"> <img class="image" src="https://picsum.photos/800/301/?random" alt="5" /><h2 className="centered">Image 5</h2></div>   ];
     return (
         <div className="main">
                <div> <Carousel slides={slides}/></div>
